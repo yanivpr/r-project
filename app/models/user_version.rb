5941e1066c74d2fdf0375aa41851e344c2fe3fd3
yaniv preiss <yanivpr@gmail.com>
2014-07-18 18:57:44 +0300

    remove comments

--- app/models/user_version.rb
+++ app/models/user_version.rb
@@ -1,8 +1,4 @@
 class UserVersion < ActiveRecord::Base
   belongs_to :user
   belongs_to :version
-
-  #belongs_to :author, class_name: User.name
-  #belongs_to :maintainer, class_name: User.name
-
 end
--- app/models/version.rb
+++ app/models/version.rb
@@ -4,8 +4,4 @@ class Version < ActiveRecord::Base
   has_many :user_versions
   has_many :users, through: :user_versions
 
-  #  has_many :maintainers, through: :user_versions, source: :user, source_type: 'Maintainer'
-  #has_many :authors, through: :user_versions, source: :user, source_type: 'Author'
-
-
 end
